0bce6ddca9f0f983fd1776b99610093e857f5a1f
Billy <105440324+BillyVII@users.noreply.github.com>
2022-12-08 16:09:02 +0100

    ConnectionScreen: mise en place v1 imageLottie nomUtilisateur motDePasse

--- app/src/main/java/pfa/technipixl/pingfest/ui/screen/ConnectionScreen.kt
+++ app/src/main/java/pfa/technipixl/pingfest/ui/screen/ConnectionScreen.kt
@@ -6,28 +6,39 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
+import pfa.technipixl.pingfest.R
 
 @Composable
 fun ConnectionScreen() {
@@ -73,17 +84,15 @@ fun TabBarItem(title:String, modifier: Modifier=Modifier, isSelected :Boolean = 
     }
 }
 
+
+
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 private fun ContentView() {
     val pagerState = rememberPagerState()
     Column() {
         SegmentedText()
-        Box(
-            modifier = Modifier
-                //.fillMaxSize()
-                .background(Color.Yellow)
-        ) {
+        Box() {
             PagerContent(pagerState = pagerState)
         }
     }
@@ -113,42 +122,37 @@ private fun PagerContent(pagerState: PagerState) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ViewOne() {
-    Column(
-        modifier = Modifier
-            //.fillMaxSize()
-            .background(Color.Magenta),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Toto",
-            fontWeight = FontWeight.Bold,
-            color = Color.Cyan,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+    Column() {
+        var text by remember { mutableStateOf("")}
+        var password by rememberSaveable { mutableStateOf("")}
+        val composition by rememberLottieComposition(
+            spec =  LottieCompositionSpec.Url("https://assets6.lottiefiles.com/packages/lf20_6aYlBl.json")
         )
-
+       LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
+       OutlinedTextField(
+           value = text,
+           onValueChange = {text = it},
+           label = {Text("Nom utilisateur")}
+       )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = {Text("Mot de passe")},
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ViewTwo() {
-    Column(
-        modifier = Modifier
-            //.fillMaxSize()
-            .background(Color.Cyan),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Tutu",
-            fontWeight = FontWeight.Bold,
-            color = Color.Magenta,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
-        )
+    Column() {
+
+
     }
 }
 
