50a1c9fd6bcd8d4e723b4d2fbeb18e8df72e8d6e
Mavrik <r.mavrik@gmail.com>
2022-12-15 14:20:04 +0100

    ajout de faux festivaux

--- app/src/main/java/pfa/technipixl/pingfest/model/Fest.kt
+++ app/src/main/java/pfa/technipixl/pingfest/model/Fest.kt
@@ -1,20 +1,50 @@
 package pfa.technipixl.pingfest.model
 
+import android.os.SystemClock.sleep
 import androidx.compose.runtime.mutableStateListOf
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import pfa.technipixl.pingfest.network.FirebaseStorageService
 import pfa.technipixl.pingfest.network.FirestoreService
 import kotlin.reflect.KProperty
 
 class Fest {
 	private val service = FirestoreService()
+	private val storage = FirebaseStorageService()
 
 	private var _list = mutableStateListOf<FestResults>()
 	val list get() = _list.toList()
 
 	init {
 		fetchFests()
+
+		repeat(50) {
+			CoroutineScope(Dispatchers.IO).launch {
+				sleep((it*100).toLong())
+				createFakeFest()
+			}
+		}
+	}
+
+	var tempI = 0
+	private suspend fun createFakeFest() {
+		val orga = mutableListOf(FestProvider.getRandomOrgaName())
+		repeat((0..3).random()) {
+			orga.add(FestProvider.getRandomOrgaName())
+		}
+		val newFest = FestResults(
+			adress = FestProvider.getRandomAdress(),
+			challenge = FestProvider.getRandomChallenge(),
+			description = FestProvider.getRandomDescription(),
+			name = FestProvider.getRandomName(),
+			organisator = orga
+		)
+		storage.getStoredImage("FestImage/Fest${(0..3).random()}.jpg") {
+			newFest.image = it.toString()
+			createNewFest(newFest)
+			tempI++
+		}
 	}
 
 	fun fetchFests() {
@@ -40,3 +70,22 @@ class Fest {
 	}
 }
 
+object FestProvider {
+	private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ' '
+
+	private fun getRandomString(size: Int = 5) : String {
+		var str = ""
+
+		repeat (size) {
+			str += charPool.random()
+		}
+
+		return str.trim()
+	}
+
+	fun getRandomAdress() = getRandomString(15)
+	fun getRandomChallenge() = getRandomString(20)
+	fun getRandomDescription() = getRandomString(50)
+	fun getRandomName() = getRandomString((5..15).random())
+	fun getRandomOrgaName() = getRandomName()
+}
--- app/src/main/java/pfa/technipixl/pingfest/model/FestResults.kt
+++ app/src/main/java/pfa/technipixl/pingfest/model/FestResults.kt
@@ -1,15 +1,17 @@
 package pfa.technipixl.pingfest.model
 
+import java.util.*
+
 data class FestResults(
     val adress: String = "", // {{faker 'address.streetAddress'}},{{faker 'address.city'}}
     val albumPhoto: List<String> = listOf(),
     val challenge: String = "", // {{faker 'animal.cat'}}
-    val dateDebut: String = "", // {{faker 'date.past'}}
-    val dateFin: String = "", // {{faker 'date.past'}}
+    val dateDebut: Long = Date().time, // {{faker 'date.past'}}
+    val dateFin: Long = Date().time + 10_000, // {{faker 'date.past'}}
     val description: String = "", // {{faker 'lorem.paragraph'}}
-    val image: String? = null, // {{faker 'internet.avatar'}}
-    val listeParticipators: List<String> = listOf(),
+    var image: String? = null, // {{faker 'internet.avatar'}}
+    val participators: List<String> = listOf(),
     val name: String = "", // {{faker 'word.adjective'}},{{faker 'music.genre'}}
-    val organisator: String? = null, // {{faker 'company.companyName'}}
+    val organisator: List<String> = listOf(), // {{faker 'company.companyName'}}
     val type: List<MusicGenre> = listOf() // {{faker 'music.genre'}}
 )
